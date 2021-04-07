a292b372bd1d829ac5b78f4e44c74155cdf21c5f
imsoumya18 <soumyadeep184@gmail.com>
2021-04-07 21:26:47 +0530

    changes

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ for file in os.listdir(os.getcwd() + '\\sheets'):
 upc_set = set(upc_list)
 
 ws2.cell(1, 1).value = 'UPC'
-ws2.cell(1, 2).value = 'Count'
+ws2.cell(1, 2).value = 'Total Count'
+ws2.cell(1, 3).value = 'Boxes'
+
 j = 2
 for i in upc_set:
     ws2.cell(j, 1).value = i
@@ -30,8 +32,21 @@ for i in upc_set:
     if upc_list.count(i) != 1:
         ws2.cell(j, 2).value = 'x' + str(upc_list.count(i))
         ws2.cell(j, 2).alignment = Alignment(horizontal='right')
+    for k in range(1, 100):
+        if ws.cell(2, k).value == 'UPC':
+            l = 3
+            while ws.cell(l, k).value is not None:
+                if ws.cell(l, k).value == i:
+                    if ws2.cell(j, 3).value is not None:
+                        ws2.cell(j, 3).value += ws.cell(1, k).value[-1] + '   '
+                    else:
+                        ws2.cell(j, 3).value = ws.cell(1, k).value[-1] + '   '
+                # break
+                l += 1
     j += 1
 
 ws2.column_dimensions['A'].width = 18.22
+ws2.column_dimensions['B'].width = 18.22
+ws2.column_dimensions['C'].width = 40
 
 wb2.save('output.xlsx')
