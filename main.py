c8cd55aabf6c3849a5dba55a388b13c56289b7f5
imsoumya18 <soumyadeep184@gmail.com>
2021-12-29 19:50:58 +0530

    change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import openpyxl
-from openpyxl.styles import Alignment
+from openpyxl.styles import Alignment, Font, PatternFill
 import os
 
 upc_list = []
@@ -12,40 +12,53 @@ for file in os.listdir(os.getcwd() + '\\sheets'):
     ws = wb.worksheets[0]
     ws2 = wb2.create_sheet(title='Sheet1', index=0)
 
-    for i in range(1, 100):
-        if ws.cell(2, i).value == 'UPC':
-            j = 3
-            while ws.cell(j, i).value is not None:
-                upc_list.append(ws.cell(j, i).value)
-                j += 1
+    i = 1
+    while ws.cell(i, 1).value is not None:
+        upc_list.append(ws.cell(i, 1).value)
+        i += 1
 
 upc_set = set(upc_list)
 
+redFill = PatternFill(start_color='FFFF0000',
+                   end_color='FFFF0000',
+                   fill_type='solid')
+
 ws2.cell(1, 1).value = 'UPC'
-ws2.cell(1, 2).value = 'Total Count'
+ws2.cell(1, 1).font = Font(size=20)
+ws2.cell(1, 1).alignment = Alignment(horizontal='center')
+ws2.cell(1, 1).fill = redFill
+ws2.cell(1, 2).value = 'Count'
+ws2.cell(1, 2).font = Font(size=20)
+ws2.cell(1, 2).alignment = Alignment(horizontal='center')
+ws2.cell(1, 2).fill = redFill
 ws2.cell(1, 3).value = 'Boxes'
+ws2.cell(1, 3).font = Font(size=20)
+ws2.cell(1, 3).alignment = Alignment(horizontal='center')
+ws2.cell(1, 3).fill = redFill
 
 j = 2
 for i in upc_set:
+    box = set()
     ws2.cell(j, 1).value = i
     ws2.cell(j, 1).number_format = '0'
     if upc_list.count(i) != 1:
         ws2.cell(j, 2).value = 'x' + str(upc_list.count(i))
         ws2.cell(j, 2).alignment = Alignment(horizontal='right')
-    for k in range(1, 100):
-        if ws.cell(2, k).value == 'UPC':
-            l = 3
-            while ws.cell(l, k).value is not None:
-                if ws.cell(l, k).value == i:
-                    if ws2.cell(j, 3).value is not None:
-                        ws2.cell(j, 3).value += ws.cell(1, k).value[-1] + '   '
-                    else:
-                        ws2.cell(j, 3).value = ws.cell(1, k).value[-1] + '   '
-                l += 1
+    k = 1
+    while ws.cell(k, 1).value is not None:
+        if ws.cell(k, 1).value == i:
+            box.add(ws.cell(k, 2).value)
+        k += 1
+    ws2.cell(j, 3).value = ', '.join(sorted(box))
+    ws2.cell(j, 3).alignment = Alignment(wrap_text=True, horizontal='right')
     j += 1
 
-ws2.column_dimensions['A'].width = 18.22
-ws2.column_dimensions['B'].width = 18.22
-ws2.column_dimensions['C'].width = 40
+ws2.column_dimensions['A'].width = 16
+ws2.column_dimensions['B'].width = 16
+ws2.column_dimensions['C'].width = 16
 
-wb2.save('output.xlsx')
+try:
+    wb2.save('output.xlsx')
+    print('Successful')
+except:
+    print('Not successful')
